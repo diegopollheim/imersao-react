386a98baa7f99b3a816ec108a37ca3d5ccd46eec
Diego <diego@moreto.co>
2022-07-31 20:57:47 -0300

    update db supabase

---
+++ src/components/Loading.jsx
@@ -0,0 +1,24 @@
+import { Backdrop, Box, CircularProgress } from "@mui/material";
+import appConfig from "../../config.json";
+
+export default function Loading() {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage: `url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"],
+      }}
+    >
+      <Backdrop sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }} open={true}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </Box>
+  );
+}
