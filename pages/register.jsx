56549d7c12613305890637c5eca42a594423f62c
Diego Pollheim <diegopollheim@gmail.com>
2022-08-02 22:32:59 -0300

    bg add register

--- pages/register.jsx
+++ pages/register.jsx
@@ -1,9 +1,10 @@
 import {LoadingButton} from "@mui/lab";
-import {Button, Stack, TextField, Typography} from "@mui/material";
+import {Button, Card, Container, Stack, TextField, Typography} from "@mui/material";
 import {useState} from "react";
 import {useForm} from "react-hook-form";
 import {toast, ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import appConfig from "../config.json";
 import api from "../src/services/api";
 
 export default function Page() {
@@ -35,31 +36,51 @@ export default function Page() {
 
   return (
     <>
-      <Stack sx={{minHeight: "100vh", backgroundColor: "backgroundColor"}}>
-        <Stack
-          component="form"
-          onSubmit={handleSubmit(registrar)}
-          margin="auto"
-          spacing={3}
-          maxWidth={500}
-        >
-          <Typography variant="h5" sx={{color: "text.primary"}}>
-            Informe seus dados para fazer o registro
-          </Typography>
-          <TextField {...register("email")} label="Email" placeholder="Seu melhor email" required />
-          <TextField
-            {...register("password")}
-            label="Senha"
-            placeholder="Mínimo 6 dígitos"
-            required
-          />
-          <Stack spacing={2}>
-            <LoadingButton loading={load} variant="contained" type="submit" color="success">
-              Registrar
-            </LoadingButton>
-            <Button variant="contained">Cancelar</Button>
+      <Stack
+        sx={{
+          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage:
+            "url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply",
+        }}
+      >
+        <Container maxWidth="md">
+          <Stack justifyContent="center" alignItems="center" sx={{minHeight: "100vh"}}>
+            <Card sx={{px: 4, py: 6, width: "fit-content"}}>
+              <Stack
+                component="form"
+                onSubmit={handleSubmit(registrar)}
+                margin="auto"
+                spacing={3}
+                maxWidth={500}
+              >
+                <Typography variant="h5" sx={{color: "text.primary"}}>
+                  Informe seus dados para fazer o registro
+                </Typography>
+                <TextField
+                  {...register("email")}
+                  label="Email"
+                  placeholder="Seu melhor email"
+                  required
+                />
+                <TextField
+                  {...register("password")}
+                  label="Senha"
+                  placeholder="Mínimo 6 dígitos"
+                  required
+                />
+                <Stack spacing={2}>
+                  <LoadingButton loading={load} variant="contained" type="submit" color="success">
+                    Registrar
+                  </LoadingButton>
+                  <Button variant="contained">Cancelar</Button>
+                </Stack>
+              </Stack>
+            </Card>
           </Stack>
-        </Stack>
+        </Container>
         <ToastContainer />
       </Stack>
     </>
